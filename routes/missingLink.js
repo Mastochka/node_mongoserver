b991fb8d262bcf1a0e84d3667d9f31e7779bda93
Mikhail Safonov <se1dom@yandex.ru>
2020-06-15 15:57:11 +0300

    missinglink fixed

--- routes/missingLink.js
+++ routes/missingLink.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
+const NotFoundError = require('../errors/not-found-err');
 
 module.exports = router;
-router.all('/', (req, res) => {
-  res.status(404).send({ message: 'Несуществующий адрес' });
-});
+router.all('/', (req, res, next) => Promise.reject(new NotFoundError('Не существующий адрес'))
+  .catch(next));
